e75549f9522920550db6beccda15cd6963900800
Maycon Godoy <mykooon@hotmail.com>
2022-10-13 11:03:46 -0300

    Coloquei nosso header

---
+++ src/pages/Main/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header/header';
+import { Container } from './styles';
+import '../../styles/global'
+
+function Main() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
+
+export default Main;
