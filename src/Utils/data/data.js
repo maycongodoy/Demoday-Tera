c91dcaf181c3a63a08d74087235bafa721c4f8ba
Adams Castillo <adams.santos.ap@gmail.com>
2022-10-26 10:17:52 -0300

    cont carrinho
    cont carrinho

--- src/Utils/data/data.js
+++ src/Utils/data/data.js
@@ -1,103 +1,113 @@
-import Product1 from '../../Utils/ImgCards/product1.png'
-import Product2 from '../../Utils/ImgCards/product2.png'
-import Product3 from '../../Utils/ImgCards/product3.png'
-import Product4 from '../../Utils/ImgCards/product4.png'
-import Product5 from '../../Utils/ImgCards/product5.png'
-import Product6 from '../../Utils/ImgCards/product6.png'
-import Product7 from '../../Utils/ImgCards/product7.png'
-import Product8 from '../../Utils/ImgCards/product8.png'
-import Product9 from '../../Utils/ImgCards/product9.png'
-import Product10 from '../../Utils/ImgCards/product10.png'
-import Product11 from '../../Utils/ImgCards/product11.png'
-import Product12 from '../../Utils/ImgCards/product12.png'
-
+import Product1 from "../../Utils/ImgCards/product1.png";
+import Product2 from "../../Utils/ImgCards/product2.png";
+import Product3 from "../../Utils/ImgCards/product3.png";
+import Product4 from "../../Utils/ImgCards/product4.png";
+import Product5 from "../../Utils/ImgCards/product5.png";
+import Product6 from "../../Utils/ImgCards/product6.png";
+import Product7 from "../../Utils/ImgCards/product7.png";
+import Product8 from "../../Utils/ImgCards/product8.png";
+import Product9 from "../../Utils/ImgCards/product9.png";
+import Product10 from "../../Utils/ImgCards/product10.png";
+import Product11 from "../../Utils/ImgCards/product11.png";
+import Product12 from "../../Utils/ImgCards/product12.png";
 
 const ArrayTennis = [
-    {
-        nome:"Tênis Nike Multi Inteligente",
-        tipo:"Tênis de corrida masculino",
-        preco:"R$ 250.00",
-        image: Product1,
-        color: "amarelo"
-    },
-    {
-        nome:"Nike Força Aérea Verde",
-        tipo:"Tênis de corrida masculino",
-        preco:"R$ 200.00",
-        image: Product2,
-        color: "verde"
-    },
-    {
-        nome:"Nike asr Adpt BB Rose",
-        tipo:"Tênis de corrida masculino",
-        preco:"R$ 150.00",
-        image: Product3,
-        color: "vermelho"
-    },
-    {
-        nome:"Nike Air Premium",
-        tipo:"Tênis de corrida masculino",
-        preco:"R$ 180.00",
-        image: Product4,
-        color: "rose"
-    },
-    {
-        nome:"Nike Adaptar BB Pro",
-        tipo:"Tênis de corrida masculino",
-        preco:"R$ 190.00",
-        image: Product5,
-        color: "marron"
-    },
-    {
-        nome:"Air Jordan PR3",
-        tipo:"Tênis de corrida masculino",
-        preco:"R$ 200.00",
-        image: Product6,
-        color: "azul-marinho"
-    },
-    {
-        nome:"Nike Air Low Premium",
-        tipo:"Tênis de corrida masculino",
-        preco:"R$ 210.00",
-        image: Product7,
-        color:"azul-escuro"
-    },
-    {
-        nome:"Nike Zoom Max",
-        tipo:"Tênis de corrida masculino",
-        preco:"R$ 320.00",
-        image: Product8,
-        color:"azul-black"
-    },
-    {
-        nome:"Nike Jordan Air Max",
-        tipo:"Tênis de corrida masculino",
-        preco:"R$ 160.00",
-        image: Product9,
-        color: "marron-claro"
-    },
-    {
-        nome:"Nike Old Max-x",
-        tipo:"Tênis de corrida masculino",
-        preco:"R$ 185.00",
-        image: Product10,
-        color: "roxo"
-    },
-    {
-        nome:"Nike Air Black Max",
-        tipo:"Tênis de corrida masculino",
-        preco:"R$ 190.00",
-        image: Product11,
-        color: "preto"
-    },
-    {
-        nome:"Nike Lime Jordan 11",
-        tipo:"Tênis de corrida masculino",
-        preco:"R$ 400.00",
-        image: Product12,
-        color: "verde"
-    },
-
-]
+  {
+    id: "0p0x1",
+    nome: "Tênis Nike Multi Inteligente",
+    tipo: "Tênis de corrida masculino",
+    preco: "R$ 250.00",
+    image: Product1,
+    color: "amarelo",
+  },
+  {
+    id: "0M0x1",
+    nome: "Nike Força Aérea Verde",
+    tipo: "Tênis de corrida masculino",
+    preco: "R$ 200.00",
+    image: Product2,
+    color: "verde",
+  },
+  {
+    id: "0M0x2",
+    nome: "Nike asr Adpt BB Rose",
+    tipo: "Tênis de corrida masculino",
+    preco: "R$ 150.00",
+    image: Product3,
+    color: "vermelho",
+  },
+  {
+    id: "0M0x3",
+    nome: "Nike Air Premium",
+    tipo: "Tênis de corrida masculino",
+    preco: "R$ 180.00",
+    image: Product4,
+    color: "rose",
+  },
+  {
+    id: "0M0x4",
+    nome: "Nike Adaptar BB Pro",
+    tipo: "Tênis de corrida masculino",
+    preco: "R$ 190.00",
+    image: Product5,
+    color: "marron",
+  },
+  {
+    id: "0M0x11",
+    nome: "Air Jordan PR3",
+    tipo: "Tênis de corrida masculino",
+    preco: "R$ 200.00",
+    image: Product6,
+    color: "azul-marinho",
+  },
+  {
+    id: "0M0x10",
+    nome: "Nike Air Low Premium",
+    tipo: "Tênis de corrida masculino",
+    preco: "R$ 210.00",
+    image: Product7,
+    color: "azul-escuro",
+  },
+  {
+    id: "0M0x9",
+    nome: "Nike Zoom Max",
+    tipo: "Tênis de corrida masculino",
+    preco: "R$ 320.00",
+    image: Product8,
+    color: "azul-black",
+  },
+  {
+    id: "0M0x8",
+    nome: "Nike Jordan Air Max",
+    tipo: "Tênis de corrida masculino",
+    preco: "R$ 160.00",
+    image: Product9,
+    color: "marron-claro",
+  },
+  {
+    id: "0M0x7",
+    nome: "Nike Old Max-x",
+    tipo: "Tênis de corrida masculino",
+    preco: "R$ 185.00",
+    image: Product10,
+    color: "roxo",
+  },
+  {
+    id: "0M0x6",
+    nome: "Nike Air Black Max",
+    tipo: "Tênis de corrida masculino",
+    preco: "R$ 190.00",
+    image: Product11,
+    color: "preto",
+  },
+  {
+    id: "0M0x5",
+    nome: "Nike Lime Jordan 11",
+    tipo: "Tênis de corrida masculino",
+    preco: "R$ 400.00",
+    image: Product12,
+    color: "verde",
+  },
+];
 
 export default ArrayTennis;
