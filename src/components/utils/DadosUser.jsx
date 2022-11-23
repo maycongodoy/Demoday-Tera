ba6117a0cc9ea98c6bc8a39263725ed76b42cd40
Adams Castillo <adams.santos.ap@gmail.com>
2022-11-23 18:23:03 -0300

    reload modificado

--- src/components/utils/DadosUser.jsx
+++ src/components/utils/DadosUser.jsx
@@ -42,7 +42,7 @@ export default function DadosUser() {
 
         localStorage.clear();
         alert("usuario deletado");
-        window.location.reload() 
+        window.location.assign("/")
 
       });
 
